b4a7a7599433de92d0016078111abe361df1f31c
maulinashintade <42725367+maulinashintade@users.noreply.github.com>
2018-10-01 20:55:49 +0700

    Update prosesLogin.php

--- prosesLogin.php
+++ prosesLogin.php
@@ -1,7 +1,16 @@
 <?php 
+session_start();
 
+$user = array(
+		"username"	=> "shinta",
+		"password"	=> "12345678"
+);
+if ($user["username"] == $_POST["username"]) && $user["password"] == $_POST["password"] {
+	$_SESSION["username"] = $user["username"];
+	$_SESSION["user"] = $user;
+	header("Location: login.php")
+}else{
+	header("Location: login.php")
+}
 
-
-
-
-?>
+ ?>
