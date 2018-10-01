dc9099fb77d743b82f8b76db70acd83739f93023
maulinashintade <42725367+maulinashintade@users.noreply.github.com>
2018-10-01 20:55:15 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,30 @@
-<?php
-
-
-
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form action="registrasi.php" method="post">
+	<center>
+		<h2>Silahkan LogIn Terlebih Dahulu!</h2><br>
+		<table>
+			<tr>
+				<td>Username</td>
+				<td>:</td>
+				<td><input type="text" name="username" placeholder="Username"></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td>:</td>
+				<td><input type="password" name="password" placeholder="Password"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td><input type="submit" name="submit" value="LogIn"></td>
+			</tr>
+		</table>
+	</center>
+</form>
+</body>
+</html>
